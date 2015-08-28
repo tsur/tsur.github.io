004b827139ad8d79bf5f830b6edbf3bb0f2503b8
Tsur <prucheta@gmail.com>
2015-08-29 01:01:15 +0200

    added whish item

--- media/applications/wishlist/wishlist.js
+++ media/applications/wishlist/wishlist.js
@@ -2,10 +2,12 @@ function Wishlist(shell) {
 
   var wishes = {
 
-    '1': 'A wife. I would like to make a family.',
-    '2': 'Funcional Programming Books (Specially about Haskell) ',
+    '1': 'A wife. I would like to have my own family.',
+    '2': 'Web Animation Using JavaScript: Develop & Design, Julian Shapiro.',
+    //'3': 'Web Components in Action, Chris Bucket',
+    '3': 'Physics for JavaScript Games, Animation, and Simulations: with HTML5 Canvas'
 
-  }
+  };
 
   var output = '\n\nIf you think you can make me happy, use the ping command to ping me with the wishlist item number you can make for real ;)\n\n\n[table class="tab"]';
 

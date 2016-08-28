25dbf24a362fcfc9cdee330f1a886a491c6bccd7
Tsur <prucheta@gmail.com>
2016-08-28 16:30:55 +0200

    updated twitter username

--- media/applications/ping/ping.js
+++ media/applications/ping/ping.js
@@ -4,7 +4,7 @@ function Ping(shell) {
 
     "email": "[link=mailto:zurisadai.pabon@gmail.com]zurisadai.pabon@gmail.com[/link]",
     "phone": "[link=callto:+34661188615]+34-661-188-615[/link]",
-    "twitter": "[link=https://twitter.com/tsurshaddai]@tsurshaddai[/link]"
+    "twitter": "[link=https://twitter.com/zuripabon]@zuripabon[/link]"
   };
 
   var output = 'Ping me to some of the contact methods below\n\n[table class="tab"]';
